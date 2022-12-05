a209b3160e08bf105394e3e5176159968a36f98a
Darren Chen <darren.chen@ryerson.ca>
2022-12-05 15:56:04 -0500

    added more stuff

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       <p> Then, I ran the command to create the react app: npx create-react-app lab10c in the terminal.  </p>
       <p> Then, I ran the command npm start to run the react app locally.  </p>
       <p> I also then created a simple counter component in react. </p>
-      <p> Finally, I edited App.js file to put in the content for this page.  </p>
-
+      <p> Then, I edited App.js file to put in the content for this page.  </p>
+      <p> Finally, I ran the command npm run deploy to build the application and deploy to github pages. </p>
     </div>
     <div>
       <img src={logo} className="App-logo" alt="logo" />
@@ -26,7 +26,10 @@ function App() {
       <p> When installing git I encountered many errors</p>
       <p> I googled what the errors might be and looked for solutions. </p>
       <p> I found a solution that fixed my installer on stackoverflow. It turns out there was something wrong with my computer! </p>
-      <p> P.S This assignment also helped me fix my other configuration related issues on my computer. </p>
+      <p> P.S This assignment also helped me fix my other configuration related issues on my computer. </p> <br/>
+      <p> Another problem I encountered was deployment issues. I had to learn how to build and deploy to github pages</p>
+      <p> I had problems with the package.json file and had to change some values such as private to false.</p>
+      <p> Also forgot to change the homepage value in the package.json file to my github.io link </p>
     </div>
     <p>Counter: {count} </p>
       <button onClick={() => setCount(count - 1)}>
