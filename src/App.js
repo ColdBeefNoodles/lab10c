cb84a38f03096c19bd9d8d7cc63cbc5a77ea6371
Darren Chen <darren.chen@ryerson.ca>
2022-12-05 15:23:44 -0500

    My First React app

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+    <div className="App">
+      <h1> How I created my first react app! </h1>
+      <p> First, I installed node.js on my computer </p>
+      <p> Next, I installed git for windows on my computer </p>
+      <p> Then, I installed VS code on my computer! </p>
+      <p> After, I made a folder for the react app. </p>
+      <p> Then, I ran the command to create the react app: npx create-react-app lab10c in the terminal.  </p>
+      <p> Then, I ran the command npm start to run the react app locally.  </p>
+      <p> I also then created a simple counter component in react. </p>
+      <p> Finally, I edited App.js file to put in the content for this page.  </p>
+
+    </div>
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+    <div className = "App">
+      <h1> The difficulties I encountered </h1>
+      <p> When installing git I encountered many errors</p>
+      <p> I googled what the errors might be and looked for solutions. </p>
+      <p> I found a solution that fixed my installer on stackoverflow. It turns out there was something wrong with my computer! </p>
+      <p> P.S This assignment also helped me fix my other configuration related issues on my computer. </p>
+    </div>
+    <p>Counter: {count} </p>
+      <button onClick={() => setCount(count - 1)}>
+        Click to count down
+      </button>
+      <button onClick={() => setCount(count + 1)}>
+        Click to count up
+      </button>
+    <footer> <h3> Darren Chen CPS530 </h3></footer>
+    </>
+  );
+}
+
+export default App;
